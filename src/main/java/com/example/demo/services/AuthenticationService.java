082423c53d037b7ce81d13f48b962027a12852a3
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-12-10 16:03:12 -0500

    authentication Service updated

--- src/main/java/com/example/demo/services/AuthenticationService.java
+++ src/main/java/com/example/demo/services/AuthenticationService.java
@@ -1,8 +1,44 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User showUser(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(Long id, User userToUpdate) {
+        //User originalUser = userRepository.findById(id).get();
+        //return repository.save(originalUser);
+        return null;
+    }
+
+    public Boolean delete(Long id) {
+        userRepository.deleteById(id);
+        return true;
+    }
+
+    public User existingUserCheck (Long id) {
+        return null;
+    }
+
+
+
 
 }
--- src/main/java/com/example/demo/services/GoalService.java
+++ src/main/java/com/example/demo/services/GoalService.java
@@ -1,8 +1,21 @@
 package com.example.demo.services;
 
+import com.example.demo.repositories.GoalAccountRepository;
+import com.example.demo.repositories.SavingGoalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GoalService {
+
+    @Autowired
+    GoalAccountRepository goalAccountRepository;
+
+    @Autowired
+    SavingGoalRepository savingGoalRepository;
+
+
+
+
 
 }
