44acec7acfe16ed69c7bace285a59436ec1da3e8
Christopher Santana <chris@zipcoders-MacBook-Pro-6.local>
2019-12-10 16:06:34 -0500

    doing investmentServices

--- src/main/java/com/example/demo/services/InvestmentService.java
+++ src/main/java/com/example/demo/services/InvestmentService.java
@@ -1,4 +1,31 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.accounts.Investment;
+import com.example.demo.repositories.InvestmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class InvestmentService {
+
+    @Autowired
+    private InvestmentRepository investmentRepository;
+
+
+    public InvestmentService(InvestmentRepository investmentRepository) {
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
