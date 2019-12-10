4476c925b3abe4a1bc6aba3741649a3211a63ae9
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-6.local>
2019-12-10 16:37:32 -0500

    Completed StatementService

--- src/main/java/com/example/demo/services/StatementService.java
+++ src/main/java/com/example/demo/services/StatementService.java
@@ -2,6 +2,52 @@ package com.example.demo.services;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.repositories.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+import com.example.demo.entities.reports.Statement;
+
 @Service
 public class StatementService {
+    @Autowired
+    CheckingRepository checkingRepository;
+
+    @Autowired
+    SavingsRepository savingsRepository;
+
+    @Autowired
+    InvestmentRepository investmentRepository;
+
+    @Autowired
+    GoalAccountRepository goalAccountRepository;
+
+    @Autowired
+    TransactionRepository transactionRepository;
+
+
+    public  StatementService() {
+    }
+
+    public Statement getStatementForAccount(Long accountId, Date date){
+        return null;
+    }
+
+    public Statement getStatementForUser(Long userId, Date date){
+        return null;
+    }
+
+    public Statement create(Statement statement){
+        return null;
+    }
+
+    public Statement update(long id, Statement newStatementData){
+        return null;
+    }
+
+    public Boolean delete(long id) {
+        return null;
+    }
+
 }
+
