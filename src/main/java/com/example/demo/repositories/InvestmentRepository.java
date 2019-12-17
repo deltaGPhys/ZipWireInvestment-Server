0d144c1c95f1d9d76e12966d3274a81e98629895
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-6.local>
2019-12-17 15:57:37 -0500

    Commit out non compiling code

--- src/main/java/com/example/demo/repositories/InvestmentRepository.java
+++ src/main/java/com/example/demo/repositories/InvestmentRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InvestmentRepository extends CrudRepository<Investment, Long> {
-     Investment findInvestmentById();
 
 }
